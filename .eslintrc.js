92f567e2f89f729ee356859469c0d763f4710be6
Lorenzo Delgado <lnsdev@proton.me>
2023-11-26 11:31:12 +0100

    chore(build): add jest plugin to eslint configuration (#32)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:jest/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
